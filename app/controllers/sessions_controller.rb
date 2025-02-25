63866e20d7327e52f54499e3b4493433ec6509c6
Andrew Willner <andrew.willner@kellogg.northwestern.edu>
2025-02-25 20:23:06 +0000

    first part of class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,28 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email" => params["email"]})
+    
     # 2. if the user exists -> check if they know their password
-    # 3. if they know their password -> login is successful
-    # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
-  end
+    if @user != nil 
+      # 3. if they know their password -> login is successful
+      if @user["password"] == params["password"]
+        flash["notice"] = "Welcome!"
+        redirect_to "/companies"
 
-  def destroy
-    # logout the user
-    flash["notice"] = "Goodbye."
-    redirect_to "/login"
-  end
-end
+            # 4. if the user doesn't exist or they don't know their password -> login fails
+      else
+        flash["notice"] = "Nope"
+        redirect_to "/login"
+  
+      end
+  
+      
+    end
+
+    end
+      
+      
+    end
+  
+
