b092cfd394174802cbd2a5cc9733322e421a5afb
Andrew Willner <andrew.willner@kellogg.northwestern.edu>
2025-02-25 22:15:04 +0000

    left off on the tasks module

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,18 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where({"user_id" => session["user_id"]})
   end
 
   def create
+    if session["user_id"] != nil
     @task = Task.new
     @task["description"] = params["description"]
     @task.save
     redirect_to "/tasks"
+    else 
+      notice["flash"] = "Login First"
+      redirect_to "/tasks"
+    end
   end
 
   def destroy
